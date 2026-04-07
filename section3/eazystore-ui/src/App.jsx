71637df985326a1c8a429b45ac19a2efb6bb383f
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-04-07 11:10:36 -0300

    creating footer

--- section3/eazystore-ui/src/App.jsx
+++ section3/eazystore-ui/src/App.jsx
@@ -1,10 +1,13 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-
   return (
-    <Header />
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
-export default App
+export default App;
---
+++ section3/eazystore-ui/src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Built with
+      <FontAwesomeIcon
+        icon={faHeart}
+        className="footer-icon"
+        aria-hidden="true"
+      />
+      by
+      <a href="https://eazybytes.com/" target="_blank" rel="noreferer">
+        eazybytes
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
