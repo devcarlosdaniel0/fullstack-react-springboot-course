795aba7614c5ffff07cca9020529f931572da7c8
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-04-22 17:32:30 -0300

    in line styles

--- section4/eazystore-ui/src/components/Home.jsx
+++ section4/eazystore-ui/src/components/Home.jsx
@@ -3,14 +3,25 @@ import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import products from "../data/Products";
 
+const h1Styling = {
+  textAlign: "center",
+  color: "#333"
+};
+
 export default function Home() {
   return (
-    <div className="home-container">
-      <PageHeading title="Explore Eazy Stickers!">
-        Add a touch of creativity to your space with our wide range of fun and
-        unique stickers. Perfect for any occasion!
-      </PageHeading>
-      <ProductListings products={products}/>
-    </div>
+    <>
+      <h1 style={h1Styling}>Demo of global CSS from Home</h1>
+    
+      {/*<h1 className="my-heading">Demo of global CSS from Home</h1>*/}
+
+      <div className="home-container">
+        <PageHeading title="Explore Eazy Stickers!">
+          Add a touch of creativity to your space with our wide range of fun and
+          unique stickers. Perfect for any occasion!
+        </PageHeading>
+        <ProductListings products={products} />
+      </div>
+    </>
   );
 }
--- section4/eazystore-ui/src/components/footer/Footer.jsx
+++ section4/eazystore-ui/src/components/footer/Footer.jsx
@@ -1,20 +1,33 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "./footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      Built with
-      <FontAwesomeIcon
-        icon={faHeart}
-        className="footer-icon"
-        aria-hidden="true"
-      />
-      by
-      <a href="https://eazybytes.com/" target="_blank" rel="noreferer">
-        eazybytes
-      </a>
-    </footer>
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "#5B21B6",
+        }}
+      >
+        Demo of global CSS scope from footer
+      </h1>
+      {/*<h1 className="my-heading">Demo of global CSS scope from footer</h1>*/}
+
+      <footer className="footer">
+        Built with
+        <FontAwesomeIcon
+          icon={faHeart}
+          className="footer-icon"
+          aria-hidden="true"
+        />
+        by
+        <a href="https://eazybytes.com/" target="_blank" rel="noreferer">
+          eazybytes
+        </a>
+      </footer>
+    </>
   );
 }
