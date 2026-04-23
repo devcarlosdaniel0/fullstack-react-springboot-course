4597ce4cea9f3d1fc6ba263122d81fb5f8d15232
Carlos Daniel <cadanielmarchesin@gmail.com>
2026-04-23 13:37:43 -0300

    Bootstrap buttons

---
+++ section4/eazystore-ui/src/components/BootstrapButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BootstrapButton({text, type}) {
+  return (
+    <button className={`btn btn-${type}`}>{text}</button>
+  )
+}
--- section4/eazystore-ui/src/components/Home.jsx
+++ section4/eazystore-ui/src/components/Home.jsx
@@ -3,14 +3,15 @@ import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import products from "../data/Products";
 import EazyButton from "./EazyButton";
-
-const h1Styling = {
-  textAlign: "center",
-  color: "#333",
-};
+import BootstrapButton from "./BootstrapButton";
 
 export default function Home() {
   const isActive = Math.random() > 0.5;
+
+  const h1Styling = {
+    textAlign: "center",
+    color: "#333",
+  };
 
   return (
     <>
@@ -23,6 +24,16 @@ export default function Home() {
       {/*<h1 className="my-heading">Demo of global CSS from Home</h1>*/}
 
       <div className="home-container">
+        <div className="container col-6">
+          <BootstrapButton text={"Submit"} type={"primary"}/>
+          <BootstrapButton text={"Save"} type={"secondary"}/>
+          <BootstrapButton text={"Okay"} type={"success"}/>
+          <BootstrapButton text={"Cancel"} type={"danger"}/>
+          <BootstrapButton text={"Delete"} type={"warning"}/>
+          <BootstrapButton text={"Link"} type={"link"}/>
+        </div>
+        
+      
         <PageHeading title="Explore Eazy Stickers!">
           Add a touch of creativity to your space with our wide range of fun and
           unique stickers. Perfect for any occasion!
